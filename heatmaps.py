7e93c21cd32cf357841f447091813c7570ca6da1
Julian Møller <julian@julianmoeller.dk>
2010-06-16 15:50:42 +0200

    Blah 3

--- heatmaps.py
+++ heatmaps.py
@@ -14,7 +14,7 @@ def main():
   dir = os.path.dirname(os.path.abspath(__file__))
 
   p = optparse.OptionParser()
-  p.add_option('--background', '-b', help='Background image', default = dir + "whitedemo.png")
+  p.add_option('--background', '-b', help='Background image', default = dir + "\\whitedemo.png")
   p.add_option('--log', '-l', help='Log file')
   p.add_option('--dotsize', '-d', help="Size of the dots to place on the heatmap", default=15)
   p.add_option('--every', '-e', help="Parse every X log entries", default = 1)
