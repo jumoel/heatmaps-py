aa39b739ae2fd98b91e17b88f666dd150647b18e
Julian Møller <julian@julianmoeller.dk>
2010-06-16 15:15:16 +0200

    Fix for Steens problem?

--- heatmaps.py
+++ heatmaps.py
@@ -12,7 +12,7 @@ import cProfile
 
 def main():
   p = optparse.OptionParser()
-  p.add_option('--background', '-b', help='Background image', default = "whitedemo.png")
+  p.add_option('--background', '-b', help='Background image', default = "./whitedemo.png")
   p.add_option('--log', '-l', help='Log file')
   p.add_option('--dotsize', '-d', help="Size of the dots to place on the heatmap", default=15)
   p.add_option('--every', '-e', help="Parse every X log entries", default = 1)
@@ -108,7 +108,7 @@ def gendot(size, minvalue):
   return img
 
 def gendeathdot(dotsize):
-  img = Image.open("skull.png")
+  img = Image.open("./skull.png")
 
   return img.resize((dotsize, dotsize))
 
